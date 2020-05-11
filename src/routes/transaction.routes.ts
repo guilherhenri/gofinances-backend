92330640acf6d0b54affeb0e292084cac16e5d9b
Guilherme Henrique <guilherhenri12@gmail.com>
2020-05-11 10:31:33 -0300

    Feature: Registration of Transactions

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,16 @@
 import { Router } from 'express';
-
+import { uuid } from 'uuidv4';
 // import TransactionsRepository from '../repositories/TransactionsRepository';
 // import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
+const appointments = [];
+
 // const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +18,18 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    const appointment = {
+      id: uuid(),
+      title,
+      value,
+      type,
+    };
+
+    appointments.push(appointment);
+
+    return response.json(appointment);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
