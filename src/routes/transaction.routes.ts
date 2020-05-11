cf31f2627ab5446d80cf4b7b09708b850ed3f768
Guilherme Henrique <guilherhenri12@gmail.com>
2020-05-11 11:28:27 -0300

    Feature: Transaction - Services & SOLID

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
@@ -22,7 +22,11 @@ transactionRouter.post('/', (request, response) => {
   try {
     const { title, value, type } = request.body;
 
-    const transaction = transactionsRepository.create({ title, value, type });
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const transaction = createTransaction.execute({ title, value, type });
 
     return response.json(transaction);
   } catch (err) {
