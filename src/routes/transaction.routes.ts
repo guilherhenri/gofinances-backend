1bfcee81673fcd36b38b37f26755f178e406d000
Guilherme Henrique <guilherhenri12@gmail.com>
2020-05-12 15:22:06 -0300

    Chore: Routes

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,16 +1,18 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
+import CreateCategoryService from '../services/CreateCategoryService';
 
 const transactionRouter = Router();
 
-const transactionsRepository = new TransactionsRepository();
-
-transactionRouter.get('/', (request, response) => {
+transactionRouter.get('/', async (request, response) => {
   try {
-    const transactions = transactionsRepository.all();
-    const balance = transactionsRepository.getBalance();
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transactions = await transactionsRepository.find();
+    const balance = await transactionsRepository.getBalance();
 
     return response.json({ transactions, balance });
   } catch (err) {
@@ -18,15 +20,24 @@ transactionRouter.get('/', (request, response) => {
   }
 });
 
-transactionRouter.post('/', (request, response) => {
+transactionRouter.post('/', async (request, response) => {
   try {
-    const { title, value, type } = request.body;
+    const { title, value, type, category: category_name } = request.body;
 
-    const createTransaction = new CreateTransactionService(
-      transactionsRepository,
-    );
+    const createCategory = new CreateCategoryService();
 
-    const transaction = createTransaction.execute({ title, value, type });
+    const category = await createCategory.execute({
+      category_name,
+    });
+
+    const createTransaction = new CreateTransactionService();
+
+    const transaction = await createTransaction.execute({
+      title,
+      value,
+      type,
+      category_id: category.id,
+    });
 
     return response.json(transaction);
   } catch (err) {
